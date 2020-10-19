afcd2991c4ce95f098abf80897bbee6cc6e435f6
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-10-19 15:48:21 +0300

    Add lexer

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import lexer.Lexer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        final String fileName = "./res/Example.kt";
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(fileName)))) {
+            String s = br.readLine();
+            while(s != null) {
+                sb.append(s).append("\n");
+                s = br.readLine();
+            }
+        } catch (Exception e) {
+            System.err.println("File error");
+        }
+        Lexer l = new Lexer(sb.toString());
+        System.out.println(l);
+    }
+}
