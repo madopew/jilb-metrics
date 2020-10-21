f4df431c6184cb740fc2edd7226ed73e2f943182
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-10-21 19:32:12 +0300

    Hard testing for hard algorithm

--- res/Test.kt
+++ res/Test.kt
@@ -1,14 +1,34 @@
 fun main() {
-    var x = 0
-    x += 3
-    if (x == 3) {
-        x += 2
-    } else {
-        if (y == 5) {
-            x--
-        } else {
-            for (val i in 1..10)
-                y--
-        }
-    }
+    x = rand(0, 4)
+    do {
+        if (x == 0) {
+            println('A')
+        } else
+            if (x == 1)
+                println('B')
+            else
+                if (x == 2) {
+                    for (var i in 1..10)
+                        for (var j in 1..10) {
+                            println(j * 10 + i)
+                        }
+                } else
+                    if (y == 1) {
+                        if (x == 0)
+                            if (x < 5)
+                                println("obvious")
+                            else {
+                                do {
+                                    println("sukaaaaa")
+                                    if (x == 5)
+                                    //nu da pustoi if i che ty mne sdelaesh
+                                    else
+                                        if (x == 3)
+                                            println("tut 10 vlozhennost")
+                                } while (x != 5)
+                            }
+                        else
+                            println("wtf is even going on")
+                    }
+    } while(i == 0) //zdoroviy do while
 }
